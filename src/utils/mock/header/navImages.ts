7fbe0dfc8b77b9c923070eec0dad26df3be52754
Flavio <fjcf@poli.br>
2023-09-19 19:22:29 -0300

    feat: add mock values for header component

---
+++ src/utils/mock/header/navImages.ts
@@ -0,0 +1,20 @@
+export const NavImages = [
+  {
+    width: 38,
+    height: 38,
+    src: '/images/wineSearch.png',
+    alt: 'Wine search image',
+  },
+  {
+    width: 38,
+    height: 38,
+    src: '/images/wineAccount.png',
+    alt: 'Wine account image',
+  },
+  {
+    width: 38,
+    height: 38,
+    src: '/images/wineBox.png',
+    alt: 'Wine box image',
+  },
+]
---
+++ src/utils/mock/header/navItens.ts
@@ -0,0 +1,22 @@
+export const NavItensLinks = [
+  {
+    href: '/clube',
+    label: 'Clube',
+  },
+  {
+    href: '/loja',
+    label: 'Loja',
+  },
+  {
+    href: '/produtores',
+    label: 'Produtores',
+  },
+  {
+    href: '/ofertas',
+    label: 'Ofertas',
+  },
+  {
+    href: '/eventos',
+    label: 'Eventos',
+  },
+]
