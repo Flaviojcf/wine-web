4fba5f2520d803da32e8bbf4bbf2ae8f66590ec7
Flavio <fjcf@poli.br>
2023-09-20 18:20:49 -0300

    refactor: add new props

--- src/app/components/Header/NavItem.tsx
+++ src/app/components/Header/NavItem.tsx
@@ -7,8 +7,18 @@ interface INavItem {
 
 export function NavItem({ href, label }: INavItem) {
   return (
-    <Link href={href}>
-      <span className="text-gray-800 text-sm hover:text-navHoverColor transition-colors duration-200">
+    <Link
+      href={href}
+      className={`flex items-center h-[78px] ${
+        label === 'Loja'
+          ? 'border-b-2 border-b-navBorderBottom sm:border-none'
+          : ''
+      }`}
+    >
+      <span
+        className={`text-gray-800 text-sm hover:text-navHoverColor transition-colors duration-200 
+        ${label === 'Loja' ? 'text-navHoverColor' : ''}`}
+      >
         {label}
       </span>
     </Link>
