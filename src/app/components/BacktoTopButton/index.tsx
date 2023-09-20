385339d9e0f2611742b9299e4ff76e6ff0a3d58b
Flavio <fjcf@poli.br>
2023-09-20 18:19:18 -0300

    feat: add back to top component

---
+++ src/app/components/BacktoTopButton/index.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { TbArrowNarrowUp } from 'react-icons/tb'
+import { Button } from '../Button'
+import { useEffect, useCallback, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+
+export function BacktoTopButton() {
+  const [show, setShow] = useState(false)
+
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  const handleScroll = useCallback(() => {
+    if (!show && window.scrollY > 500) setShow(true)
+    if (show && window.scrollY <= 500) setShow(false)
+  }, [show])
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [handleScroll])
+
+  return (
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          className="fixed right-4 bottom-4 z-20"
+          initial={{ opacity: 0, right: -10 }}
+          animate={{ opacity: 1, right: 16 }}
+          exit={{ opacity: 0, right: -10 }}
+        >
+          <Button
+            onClick={scrollToTop}
+            className="bg-buttonColor py-3 px-4 rounded-lg text-gray-50 flex 
+            items-center justify-center gap-2 hover:opacity-80 transition-all disabled:opacity-50"
+          >
+            <TbArrowNarrowUp size={20} />
+          </Button>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  )
+}
