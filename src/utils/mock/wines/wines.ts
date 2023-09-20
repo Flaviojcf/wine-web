f60bde23648671df18b87c4baf427bc409920545
Flavio <fjcf@poli.br>
2023-09-20 18:17:58 -0300

    feat: create wines infos mock

---
+++ src/utils/mock/wines/wines.ts
@@ -0,0 +1,67 @@
+import { IWine } from '@/app/components/WineCard'
+
+export const WinesData: IWine[] = [
+  {
+    image: '/images/wines/wineImage.png',
+    name: 'Bacalhôa Meia Pipa Private Selection Castelão Syrah 2015',
+    offer: 50,
+    partnerOffer: 25,
+    price: 35,
+  },
+  {
+    image: '/images/wines/wineImage.png',
+    name: 'Bacalhôa Meia Pipa Private Selection Castelão Syrah 2016',
+    offer: 45,
+    partnerOffer: 22.5,
+    price: 42,
+  },
+  {
+    image: '/images/wines/wineImage.png',
+    name: 'Bacalhôa Meia Pipa Private Selection Castelão Syrah 2017',
+    offer: 70,
+    partnerOffer: 35,
+    price: 30,
+  },
+  {
+    image: '/images/wines/wineImage.png',
+    name: 'Bacalhôa Meia Pipa Private Selection Castelão Syrah 2018',
+    offer: 55,
+    partnerOffer: 27.5,
+    price: 45,
+  },
+  {
+    image: '/images/wines/wineImage.png',
+    name: 'Bacalhôa Meia Pipa Private Selection Castelão Syrah 2019',
+    offer: 80,
+    partnerOffer: 40,
+    price: 55,
+  },
+  {
+    image: '/images/wines/wineImage.png',
+    name: 'Bacalhôa Meia Pipa Private Selection Castelão Syrah 2020',
+    offer: 90,
+    partnerOffer: 45,
+    price: 125,
+  },
+  {
+    image: '/images/wines/wineImage.png',
+    name: 'Bacalhôa Meia Pipa Private Selection Castelão Syrah 2021',
+    offer: 110,
+    partnerOffer: 55,
+    price: 180,
+  },
+  {
+    image: '/images/wines/wineImage.png',
+    name: 'Bacalhôa Meia Pipa Private Selection Castelão Syrah 2022',
+    offer: 130,
+    partnerOffer: 65,
+    price: 280,
+  },
+  {
+    image: '/images/wines/wineImage.png',
+    name: 'Bacalhôa Meia Pipa Private Selection Castelão Syrah 2023',
+    offer: 150,
+    partnerOffer: 75,
+    price: 550,
+  },
+]
