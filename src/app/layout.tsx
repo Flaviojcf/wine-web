5149c356a8006f432d7ae47bf7a12d49fa3b2349
Flavio <fjcf@poli.br>
2023-09-20 18:21:01 -0300

    feat: add components to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Header } from './components/Header'
+import { Header } from '@/app/components/Header'
+import { BacktoTopButton } from './components/BacktoTopButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Wine Web',
   description: 'Web application for wine.com.br',
+  icons: [
+    {
+      url: '/wineLogo.ico',
+    },
+  ],
 }
 
 export default function RootLayout({
@@ -19,7 +25,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
+
         {children}
+        <BacktoTopButton />
       </body>
     </html>
   )
