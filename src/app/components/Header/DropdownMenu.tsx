0a7c918252f9f579b2c74144de7d28ec8d074714
Flavio <fjcf@poli.br>
2023-09-20 18:20:14 -0300

    refactor: add radix and framer motion

--- src/app/components/Header/DropdownMenu.tsx
+++ src/app/components/Header/DropdownMenu.tsx
@@ -1,29 +1,53 @@
+'use client'
 import { NavItensLinks } from '@/utils/mock/header/navItens'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import Image from 'next/image'
-import Link from 'next/link'
 import { NavItem } from './NavItem'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useState } from 'react'
 
 export function Menu() {
+  const [isOpen, setOpen] = useState<boolean>(false)
+
   return (
-    <DropdownMenu.Root>
+    <DropdownMenu.Root open={isOpen} onOpenChange={setOpen}>
       <DropdownMenu.Trigger className="hidden sm:flex">
-        <Image
-          src="/images/menuWine.png"
-          alt="Dropdown Menu"
-          width={21}
-          height={16}
-        />
+        <div>
+          <Image
+            src="/images/menuWine.png"
+            alt="Dropdown Menu"
+            width={21}
+            height={16}
+          />
+        </div>
       </DropdownMenu.Trigger>
-      <DropdownMenu.Content className="z-50">
-        <ul className="bg-white flex flex-col px-8 py-4 rounded-md gap-4 text-black mt-6 ml-2 ">
-          {NavItensLinks.map((item) => (
-            <li key={`label-${item.label}`}>
-              <NavItem href={item.href} label={item.label} />
-            </li>
-          ))}
-        </ul>
-      </DropdownMenu.Content>
+      <AnimatePresence>
+        {isOpen && (
+          <DropdownMenu.Content className="h-screen -mt-10" asChild forceMount>
+            <motion.ul
+              className="flex flex-col px-8 py-4 pt-16 
+              rounded-md gap-4 text-black w-[280px] h-full bg-white z-999"
+              initial={{ x: -200 }}
+              animate={{ x: 0 }}
+              exit={{ x: -300 }}
+              transition={{ duration: 0.3 }}
+            >
+              <div
+                className="w-6 h-6 relative cursor-pointer -mt-8 ml-auto"
+                onClick={() => setOpen(false)}
+              >
+                <div className="w-full h-0.5 bg-black absolute top-1/2 transform -translate-y-1/2 rotate-45"></div>
+                <div className="w-full h-0.5 bg-black absolute top-1/2 transform -translate-y-1/2 -rotate-45"></div>
+              </div>
+              {NavItensLinks.map((item) => (
+                <li key={`label-${item.label}`}>
+                  <NavItem href={item.href} label={item.label} />
+                </li>
+              ))}
+            </motion.ul>
+          </DropdownMenu.Content>
+        )}
+      </AnimatePresence>
     </DropdownMenu.Root>
   )
 }
